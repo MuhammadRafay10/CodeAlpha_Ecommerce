6e203055c1d09789d2caae812afd327a16e0f124
MuhammadRafay10 <mr.abubakar2005@gmail.com>
2026-06-10 23:30:45 -0700

    remove .evn from git tracking

--- server/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getProducts, getProductById, createProduct } = require('../controllers/productController');
-const router = express.Router();
-
-router.route('/').get(getProducts).post(createProduct);
-router.route('/:id').get(getProductById);
-
-module.exports = router;
